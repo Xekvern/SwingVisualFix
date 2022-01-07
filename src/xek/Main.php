8601a81c8b58724c970ced0275268496f069d88a
Xekvern <77306216+Xekvern@users.noreply.github.com>
2022-01-07 08:41:05 +0800

    Update Main.php

--- src/xek/Main.php
+++ src/xek/Main.php
@@ -8,13 +8,6 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase {
 
     public function onEnable(): void {
-        @mkdir($this->getDataFolder());
-		$this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents(new PacketListener($this), $this);
     }
-
-    public static function getInstance(): Main {
-		return Server::getInstance()->getPluginManager()->getPlugin("SwingFix");
-	}
-
 }
